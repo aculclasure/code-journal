a7a9643deb8417901ae61b9a42d3a82f788a9c3b
Andrew Culclasure <aculclasure@USROMACULCLAS01-2.local>
2020-06-21 11:27:14 -0400

    Add 5th iteration of exercism tournament golang solution

--- go/exercism/go/tournament/tournament.go
+++ go/exercism/go/tournament/tournament.go
@@ -15,17 +15,16 @@ type team struct {
 	wins    int
 	losses  int
 	draws   int
+	points  int
 	matches int
 }
-
-func (t *team) getPoints() int { return (3 * t.wins) + t.draws }
 
 // Tally accepts a series of football match records representing a
 // competition and returns a summary table of statistics for
 // the competition.
 func Tally(reader io.Reader, writer io.Writer) error {
 	scanner := bufio.NewScanner(reader)
-	competition := make(map[string]*team)
+	competition := make(map[string]team)
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -38,39 +37,40 @@ func Tally(reader io.Reader, writer io.Writer) error {
 			return fmt.Errorf("got invalid line input: %s", line)
 		}
 
-		team1, team2, matchResult := fields[0], fields[1], fields[2]
-		if _, ok := competition[team1]; !ok {
-			competition[team1] = &team{name: team1}
-		}
-		if _, ok := competition[team2]; !ok {
-			competition[team2] = &team{name: team2}
-		}
-		team1Record, team2Record := competition[team1], competition[team2]
-		team1Record.matches++
-		team2Record.matches++
+		a, b, matchResult := fields[0], fields[1], fields[2]
+		teamA, teamB := competition[a], competition[b]
+		teamA.name, teamB.name = a, b
+		teamA.matches++
+		teamB.matches++
 		switch matchResult {
 		case "win":
-			team1Record.wins++
-			team2Record.losses++
+			teamA.wins++
+			teamA.points += 3
+			teamB.losses++
 		case "loss":
-			team1Record.losses++
-			team2Record.wins++
+			teamA.losses++
+			teamB.wins++
+			teamB.points += 3
 		case "draw":
-			team1Record.draws++
-			team2Record.draws++
+			teamA.draws++
+			teamA.points++
+			teamB.draws++
+			teamB.points++
 		default:
 			return fmt.Errorf("got invalid match outcome: %s", matchResult)
 		}
+		competition[a] = teamA
+		competition[b] = teamB
 	}
-	allTeamRecords := make([]*team, 0, len(competition))
+	allTeamRecords := make([]team, 0, len(competition))
 	for _, v := range competition {
 		allTeamRecords = append(allTeamRecords, v)
 	}
 	sort.Slice(allTeamRecords, func(i, j int) bool {
-		if allTeamRecords[i].getPoints() == allTeamRecords[j].getPoints() {
+		if allTeamRecords[i].points == allTeamRecords[j].points {
 			return allTeamRecords[i].name < allTeamRecords[j].name
 		}
-		return allTeamRecords[i].getPoints() > allTeamRecords[j].getPoints()
+		return allTeamRecords[i].points > allTeamRecords[j].points
 	})
 	fmt.Fprintf(writer,
 		"%*s | %2s | %2s | %2s | %2s | %2s\n",
@@ -78,7 +78,7 @@ func Tally(reader io.Reader, writer io.Writer) error {
 	for _, t := range allTeamRecords {
 		fmt.Fprintf(writer,
 			"%*s | %2d | %2d | %2d | %2d | %2d\n",
-			-teamNamePadLength, t.name, t.matches, t.wins, t.draws, t.losses, t.getPoints())
+			-teamNamePadLength, t.name, t.matches, t.wins, t.draws, t.losses, t.points)
 	}
 	return nil
 }
