daef6114a1b950f83b2e38c15045cf1cc2ae1028
Andrew Culclasure <andrew.culclasure@infor.com>
2020-11-03 16:28:30 -0500

    Add 2nd iteration of flatten-array solution

--- go/exercism/go/flatten-array/flatten_array.go
+++ go/exercism/go/flatten-array/flatten_array.go
@@ -3,22 +3,15 @@ package flatten
 // Flatten accepts a list of possible nested lists and returns the
 // flattened list of items, ignoring any nil values.
 func Flatten(input interface{}) []interface{} {
-	if list, ok := input.([]interface{}); ok {
-		return flattenList(list)
-	}
-	return nil
-}
-
-func flattenList(input []interface{}) []interface{} {
 	flattened := []interface{}{}
 
-	for _, v := range input {
-		if v != nil {
-			if nestedList, ok := v.([]interface{}); ok {
-				flattened = append(flattened, flattenList(nestedList)...)
-			} else {
-				flattened = append(flattened, v)
+	if input != nil {
+		if list, ok := input.([]interface{}); ok {
+			for _, v := range list {
+				flattened = append(flattened, Flatten(v)...)
 			}
+		} else {
+			flattened = append(flattened, input)
 		}
 	}
 	return flattened
